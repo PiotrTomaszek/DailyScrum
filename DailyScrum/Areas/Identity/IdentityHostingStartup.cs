84ff330cf80648fc14a87ffda2da4b4915275d0c
Piotr T <tomaszekpiotrek@gmail.com>
2021-05-04 12:24:05 +0200

    work with identity

---
+++ DailyScrum/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DailyScrum.Areas.Identity.Data;
+using DailyScrum.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DailyScrum.Areas.Identity.IdentityHostingStartup))]
+namespace DailyScrum.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DailyScrumContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DailyScrumContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DailyScrumContext>();
+            });
+        }
+    }
+}
