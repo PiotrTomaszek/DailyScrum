be5fab780acfd363b2ee483e8920f9168761e2fb
Piotr T <tomaszekpiotrek@gmail.com>
2021-05-18 14:57:06 +0200

    to change overwie

--- DailyScrum/Areas/Identity/Pages/Account/Manage/ScrumOptions.cshtml.cs
+++ DailyScrum/Areas/Identity/Pages/Account/Manage/ScrumOptions.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DailyScrum.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,18 @@ namespace DailyScrum.Areas.Identity.Pages.Account.Manage
 {
     public class ScrumOptionsModel : PageModel
     {
+        public DailyScrumContext _context;
+
+        public ScrumOptionsModel(DailyScrumContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            //var 
+
+            //var user = _context.Users.Where()
         }
     }
 }
