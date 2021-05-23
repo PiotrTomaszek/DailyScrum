a4f48e6be62c80a93b88742500800f8e6b28a8bd
Piotr T <tomaszekpiotrek@gmail.com>
2021-05-23 21:25:12 +0200

    dummy commit

--- DailyScrum/obj/Debug/net5.0/DailyScrum.AssemblyInfo.cs
+++ DailyScrum/obj/Debug/net5.0/DailyScrum.AssemblyInfo.cs
@@ -12,6 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2d58fd9c-77ee-4c1f-a4bc-242e4fb4f7c4")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("DailyScrum")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
