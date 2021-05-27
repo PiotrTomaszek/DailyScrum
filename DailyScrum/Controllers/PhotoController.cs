dd50c3a515f118d3865e4e05a8ef3a986215468c
Piotr T <tomaszekpiotrek@gmail.com>
2021-05-27 17:06:16 +0200

    moze slaob dziala ale jakotako

--- DailyScrum/Controllers/PhotoController.cs
+++ DailyScrum/Controllers/PhotoController.cs
@@ -4,12 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Processing;
 using System;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DailyScrum.Controllers
@@ -66,17 +64,11 @@ namespace DailyScrum.Controllers
 
             var finalSize = img.Width > img.Height ? img.Height : img.Width;
 
-            var offsetX = img.Width / 4;
-            var offsetY = img.Height / 4;
+            var x = ((img.Width - 256) / 4);
+            var y = (img.Height - 256) / 4;
 
-            if (img.Width < 256)
-                offsetX = 0;
-
-            if (img.Height < 256)
-                offsetY = 0;
-
-            img.Mutate(x => x
-            .Crop(new Rectangle(offsetX, offsetY, finalSize, finalSize))
+            img.Mutate(mut => mut
+            .Crop(new Rectangle(x, 0, finalSize, finalSize))
             .Resize(256, 256));
 
             var encoder = new JpegEncoder()
@@ -97,46 +89,6 @@ namespace DailyScrum.Controllers
             await cloudBlockBlob.UploadFromByteArrayAsync(result, 0, result.Length);
 
             _userRepository.SetPhotoPathById(userId, cloudBlockBlob.Uri.AbsoluteUri);
-
-        }
-
-
-        public static Image ResizeImage(Image imgToResize, Size destinationSize)
-        {
-            var originalWidth = imgToResize.Width;
-            var originalHeight = imgToResize.Height;
-
-            //how many units are there to make the original length
-            var hRatio = (float)originalHeight / destinationSize.Height;
-            var wRatio = (float)originalWidth / destinationSize.Width;
-
-            //get the shorter side
-            var ratio = Math.Min(hRatio, wRatio);
-
-            var hScale = Convert.ToInt32(destinationSize.Height * ratio);
-            var wScale = Convert.ToInt32(destinationSize.Width * ratio);
-
-            //start cropping from the center
-            var startX = (originalWidth - wScale) / 2;
-            var startY = (originalHeight - hScale) / 2;
-
-            //crop the image from the specified location and size
-            var sourceRectangle = new Rectangle(startX, startY, wScale, hScale);
-
-            //the future size of the image
-            var bitmap = new Bitmap(destinationSize.Width, destinationSize.Height);
-
-            //fill-in the whole bitmap
-            var destinationRectangle = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
-
-            //generate the new image
-            using (var g = Graphics.FromImage(bitmap))
-            {
-                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                g.DrawImage(imgToResize, destinationRectangle, sourceRectangle, GraphicsUnit.Pixel);
-            }
-
-            return bitmap;
 
         }
     }
--- DailyScrum/Hubs/UserList.cs
+++ DailyScrum/Hubs/UserList.cs
@@ -1,9 +1,5 @@
-﻿using DailyScrum.ViewModels;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
