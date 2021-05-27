2fc31ba409b6a88dc0f47a7fa0e2be76c0a7c9f7
Piotr T <tomaszekpiotrek@gmail.com>
2021-05-27 16:46:30 +0200

    test stuff with resize from net

--- DailyScrum/Controllers/PhotoController.cs
+++ DailyScrum/Controllers/PhotoController.cs
@@ -3,7 +3,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Processing;
 using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -48,20 +53,90 @@ namespace DailyScrum.Controllers
             // to remove
 
             var url = _userRepository.GetPhotoPathById(userId);
-
-            url = url.Substring(url.LastIndexOf('/') +1 );
+            url = url.Substring(url.LastIndexOf('/') + 1);
 
             var cloudBlockBlobToDelete = cloudBlobContainer.GetBlockBlobReference(url);
-
             await cloudBlockBlobToDelete.DeleteIfExistsAsync();
+
+            // to save
+
+            var img = Image.Load(file.OpenReadStream());
+
+
+
+            var finalSize = img.Width > img.Height ? img.Height : img.Width;
+
+            var offsetX = img.Width / 4;
+            var offsetY = img.Height / 4;
+
+            if (img.Width < 256)
+                offsetX = 0;
+
+            if (img.Height < 256)
+                offsetY = 0;
+
+            img.Mutate(x => x
+            .Crop(new Rectangle(offsetX, offsetY, finalSize, finalSize))
+            .Resize(256, 256));
+
+            var encoder = new JpegEncoder()
+            {
+                Quality = 30
+            };
+
+            var memoryStream = new MemoryStream();
+
+            img.Save(memoryStream, encoder);
+
+            var result = memoryStream.ToArray();
 
             var cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(Guid.NewGuid().ToString());
 
             cloudBlockBlob.Properties.ContentType = file.ContentType;
 
-            await cloudBlockBlob.UploadFromStreamAsync(file.OpenReadStream());
+            await cloudBlockBlob.UploadFromByteArrayAsync(result, 0, result.Length);
 
             _userRepository.SetPhotoPathById(userId, cloudBlockBlob.Uri.AbsoluteUri);
+
+        }
+
+
+        public static Image ResizeImage(Image imgToResize, Size destinationSize)
+        {
+            var originalWidth = imgToResize.Width;
+            var originalHeight = imgToResize.Height;
+
+            //how many units are there to make the original length
+            var hRatio = (float)originalHeight / destinationSize.Height;
+            var wRatio = (float)originalWidth / destinationSize.Width;
+
+            //get the shorter side
+            var ratio = Math.Min(hRatio, wRatio);
+
+            var hScale = Convert.ToInt32(destinationSize.Height * ratio);
+            var wScale = Convert.ToInt32(destinationSize.Width * ratio);
+
+            //start cropping from the center
+            var startX = (originalWidth - wScale) / 2;
+            var startY = (originalHeight - hScale) / 2;
+
+            //crop the image from the specified location and size
+            var sourceRectangle = new Rectangle(startX, startY, wScale, hScale);
+
+            //the future size of the image
+            var bitmap = new Bitmap(destinationSize.Width, destinationSize.Height);
+
+            //fill-in the whole bitmap
+            var destinationRectangle = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            //generate the new image
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(imgToResize, destinationRectangle, sourceRectangle, GraphicsUnit.Pixel);
+            }
+
+            return bitmap;
 
         }
     }
